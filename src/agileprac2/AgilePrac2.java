86bf0875d3830bb9a67f4512cea4e0a447f6f641
taruc <taruc@B9>
2017-10-31 11:00:52 +0800

    my first-branch commit

--- src/agileprac2/AgilePrac2.java
+++ src/agileprac2/AgilePrac2.java
@@ -15,7 +15,8 @@ public class AgilePrac2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print
+      //first branch
+        //first branch
     }
     
 }
